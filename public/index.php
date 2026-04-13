4ec2786e66e697c9ae6eff3b5a6ae78c4c5bf3a0
Cédric <cedric.tedie@gmail.com>
2026-04-13 18:11:15 +0200

    Mise en place de l'environnement

---
+++ public/index.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$routes = require __DIR__ . '/../routes/web.php';
+
+$uri = strtok($_SERVER['REQUEST_URI'], '?');
+
+if (isset($routes[$uri])) {
+
+    $controller = $routes[$uri][0];
+    $method = $routes[$uri][1];
+
+    (new $controller())->$method();
+
+} else {
+
+    http_response_code(404);
+    echo '404';
+
+}
